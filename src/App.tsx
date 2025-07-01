356919ca1637d39e974769562747dfa51cf24247
Ahmad Shaikh <ahmadshaikh2026@gmail.com>
2025-07-01 16:34:09 +0530

    feat:added navigation using daisy ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-
+import Header from './components/Header';
 function App() {
 
   return (
     <>
-    <h1>Hello world</h1>
+    <Header/>
+    <h1 className="text-5xl bold underline">Hello world</h1>
+    <button className="btn">Hello</button>
     </>
   )
 }
